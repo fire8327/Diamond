d4e2b4fa94c01c759e4bc3b0eb4cd812290e80ba
fire8327 <fire83274@gmail.com>
2024-05-31 21:08:13 +0300

    create header

---
+++ Assets/JS/main.js
@@ -0,0 +1,12 @@
+/* меню */
+$("#toggler").click(() => {
+    $("#menu").toggleClass("max-lg:top max-lg:-translate-y-full")
+    $("#menu").toggleClass("max-lg:top-full")
+    $("#overlay").toggleClass("hidden")
+})
+
+$("#overlay").click(() => {
+    $("#menu").addClass("max-lg:top max-lg:-translate-y-full")
+    $("#menu").removeClass("max-lg:top-full")
+    $("#overlay").addClass("hidden")
+})
